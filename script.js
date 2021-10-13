8e3715d2754e67147051cf0e37f2b86bebf9a89a
Amber van der Riet <56724503+110250-coder@users.noreply.github.com>
2021-10-13 10:25:10 +0000

    bal botst tegen goals

--- script.js
+++ script.js
@@ -29,7 +29,20 @@ class Ball {
 
     spelers.forEach((speler1) => {
       if (speler1.x < this.x + this.w && speler1.x + speler1.w > this.x) {
-        if (speler1.y < this.y + this.h && speler1.y + speler1.h > this.y) {      
+        if (speler1.y < this.y + this.h && speler1.y + speler1.h > this.y) { 
+          this.colour = "orange"     
+          this.vx = this.vx * -1;
+        }
+      }
+      else {
+        this.colour = "red";
+      }
+    })
+
+    goals.forEach((goal1) => {
+      if (goal1.x < this.x + this.w && goal1.x + goal1.w > this.x) {
+        if (goal1.y < this.y + this.h && goal1.y + goal1.h > this.y) { 
+          this.colour = "orange"     
           this.vx = this.vx * -1;
         }
       }
@@ -42,17 +55,17 @@ class Ball {
 }
 
 class Goal {
-  constructor(xg, yg, wg, hg, colourg) {
-    this.xg = xg;
-    this.yg = yg;
-    this.wg = wg;
-    this.hg = hg;
-    this.colourg = ("black");
+  constructor(x, y, w, h, colour) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.colour = ("black");
   }
 
   drawGoal() {
-    fill(this.colourg);
-    rect(this.xg, this.yg, 15, 80);
+    fill(this.colour);
+    rect(this.x, this.y, 15, 80);
   }
   
   checkcollision(){
@@ -62,7 +75,6 @@ class Goal {
     if (this.x <= 0) {
       this.x = 0;
     } 
-
     if (this.y <= 0) {
       this.y = 0;
     }
@@ -129,17 +141,19 @@ function preload(){
   image = loadImage('Images/image.png');
 }
 
-var spelers = [];
+var spelers = [], goals = [];
 function setup() {
   createCanvas(500, 400);
   
   ball1 = new Ball(151, 70, 30, 30, 3, 3, this.colour);
-  goal1 = new Goal(25, 160, 20, 80, this.colourg);
-  goal2 = new Goal(457, 160, 15, 80, this.colourg);
+  goal1 = new Goal(25, 160, 20, 80, this.colour);
+  goal2 = new Goal(457, 160, 15, 80, this.colour);
   speler1 = new Speler(50, 210, "m");
   speler2 = new Speler(460, 265);
   spelers.push(speler1);
   spelers.push(speler2);
+  goals.push(goal1);
+  goals.push(goal2);
 }
 
 function draw() {
